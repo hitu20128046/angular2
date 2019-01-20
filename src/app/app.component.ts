8aeb3b91b15fc248955634765d136a7ad7962bbe
Hitesh Kumar <khitesh@qti.qualcomm.com>
2019-01-20 16:08:23 +0530

    interpolation part2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  // template: `<h1>Hello {{name}}</h1>`,
+  templateUrl:'./app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'Hitzz';
+  private username:string = 'Test';
+  private currentUser:User ={username:'Test',email:'test@test.com'}
+
+  private getUsername():string{
+    return this.currentUser.username
+  }
+}
+interface User{
+  username:string,
+  email:string
+}
