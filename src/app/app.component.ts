126304f5ca565b7c562ae8ba33bf84e602f51f3c
Hitesh Kumar <khitesh@qti.qualcomm.com>
2019-01-21 21:44:16 +0530

    commit2 check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,41 @@
 import { Component } from '@angular/core';
+import {Account} from './account.model';
 
 @Component({
   selector: 'my-app',
-  // template: `<h1>Hello {{name}}</h1>`,
-  templateUrl:'./app.component.html'
-})
-export class AppComponent  {
-  name = 'Hitzz';
-  private username:string = 'Test';
-  private currentUser:User ={username:'Test',email:'test@test.com'}
+   // template: `<h1>Hello {{name}}</h1>`,
+  templateUrl:'./app.component.html',
+  // styles: ['.blue-bg{background-color:blue;}']
 
-  private getUsername():string{
-    return this.currentUser.username
+})
+export class AppComponent {
+  private _account:Array<Account>=[
+    {
+      id:1,
+      title:"SBI Bank",
+      description:"SBI main account",
+      balance:5900000
+    },
+    new Account(2,"HDFC Bank","Saving Accont",234578)
+  ]
+
+  private _nextId = 3
+
+  private creatAcc(titleE1:any, descE1:any ,balE1:any){
+    this._account.push(new Account(
+                              this._nextId,
+                              titleE1.value,
+                              descE1.value,
+                              balE1.value
+                            ))
+      this._nextId++
+
+      titleE1.value=""
+      descE1.value=""
+      balE1.value=0
+
   }
-}
-interface User{
-  username:string,
-  email:string
+  private removeAcc(index:number){
+    this._account.splice(index,1)
+  }
 }
